9643831b3553be997fddb305cb75a68ba51eb02c
Horpey <adeniran1059@gmail.com>
2019-03-21 18:29:05 +0100

    First Upload

---
+++ assets/vendor/slick/slick-setup.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.about-cast').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        cssEase: 'linear'
+    });
+  });
